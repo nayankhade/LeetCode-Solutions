ddcd2961eaedb4289870dd9da3e2a89001136aed
Nayan Khade <nayankhade2001@gmail.com>
2022-08-16 14:01:03 +0530

    LeetCode

---
+++ Best TimetoBuyandSellStock.java
@@ -0,0 +1,10 @@
+class BestTimetoBuyandSellStock {
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        for(int i=1; i<prices.length+1)
+        
+    }
+    public static void main(String[] args){
+        
+    }
+}
